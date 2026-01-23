be93103c2f1920943ac5cdc904250847302372b0
antoninmrtl <antonin.mrtl.pr@gmail.com>
2026-01-23 14:39:42 +0100

    add packages controller

--- src/main/java/fr/eni/springboot/repository/rowMapper/UtilisateurRowMapper.java
+++ src/main/java/fr/eni/springboot/repository/rowMapper/UtilisateurRowMapper.java
@@ -12,14 +12,14 @@ public class UtilisateurRowMapper implements RowMapper<User> {
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
         User user = new User();
 
-        user.setId(rs.getInt("id"));
+        user.setUser_id(rs.getInt("user_id"));
         user.setPasword(rs.getString("pasword"));
         user.setUsername(rs.getString("username"));
         user.setEmail(rs.getString("email"));
         user.setFristName(rs.getString("fristName"));
         user.setLastName(rs.getString("lastName"));
         user.setNumPhone(rs.getString("numPhone"));
-        user.setVille(rs.getString("ville"));
+        user.setCity(rs.getString("city"));
         user.setStreet((rs.getString("street")));
         user.setCredit(rs.getInt("credit"));
         user.setPostalCode(rs.getInt("postalCode"));
