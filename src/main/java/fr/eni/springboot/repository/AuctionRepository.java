fbab36333c41294c71efc8228ab99169b07e20b5
amartel2025 <antonin.mrtl.pr@gmail.com>
2026-01-26 11:06:56 +0100

    create repository of Auction

--- src/main/java/fr/eni/springboot/repository/AuctionRepository.java
+++ src/main/java/fr/eni/springboot/repository/AuctionRepository.java
@@ -1,4 +1,15 @@
 package fr.eni.springboot.repository;
 
+import fr.eni.springboot.bo.Auction;
+
+import java.util.List;
+
 public interface AuctionRepository {
+    void createAuction(Auction auction);
+
+    List<Auction> readAuction();
+
+    void updateAuction(Auction auction);
+
+    void deleteAuction(long auction_id);
 }
--- src/main/java/fr/eni/springboot/repository/AuctionRepositorySql.java
+++ src/main/java/fr/eni/springboot/repository/AuctionRepositorySql.java
@@ -1,12 +1,16 @@
 package fr.eni.springboot.repository;
 
 import fr.eni.springboot.bo.Auction;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class AuctionRepositorySql implements AuctionRepository {
@@ -21,21 +25,44 @@ public class AuctionRepositorySql implements AuctionRepository {
 
     //CRUD
 
+    @Override
     public void createAuction(Auction auction) {
-        //GeneratedKeyHolder keyHolder = new GeneratedKeyHolder(); auction n'a pas d'id ??
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
 
         String sql = "INSERT INTO auction (auctionDate,auctionAmount, user_id, article_id) VALUES (:auctionDate,:auctionAmount,:user_id,:article_id)";
 
         BeanPropertySqlParameterSource map  = new BeanPropertySqlParameterSource(auction);
 
-        namedParameterJdbcTemplate.update(sql, map);
+        namedParameterJdbcTemplate.update(sql, map, keyHolder);
+
+        auction.setAuction_id(Objects.requireNonNull(keyHolder.getKey()).longValue());
     }
 
-    public void readAuction(Auction auction) {
-        String sql = "SELECT * FROM auction WHERE auctionId = :auctionId";
+    @Override
+    public List<Auction> readAuction() {
+        String sql = "SELECT user_id, article_id, auctionDate, auctionAmount FROM auction";
+
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Auction.class));
+    }
+
+    @Override
+    public void updateAuction(Auction auction){
+        String sql =" UPDATE auction SET user_id =:user_id, article_id=:article_id,auctionDate=:auctionDate,auctionAmount=:auctionAmount where auction_id=:auction_id";
+
+        BeanPropertySqlParameterSource map = new BeanPropertySqlParameterSource(auction);
+
+        namedParameterJdbcTemplate.queryForObject(sql, map, new BeanPropertyRowMapper<>(Auction.class));
+
+    }
+
+    @Override
+    public void deleteAuction(long auction_id){
+        String sql ="delete from auction where auction_id=:auction_id";
 
         MapSqlParameterSource map = new MapSqlParameterSource();
-        //map.addValue("");
+        map.addValue("auction_id", auction_id);
+
+        namedParameterJdbcTemplate.update(sql, map);
     }
 
 
