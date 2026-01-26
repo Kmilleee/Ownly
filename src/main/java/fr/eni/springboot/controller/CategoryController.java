7281c73d1b8fad7cda8218fd3537ae6c3a853a2b
Kmilleee <camillepayen35@gmail.com>
2026-01-26 16:18:32 +0100

    Add page listCategories (view and delete)

--- src/main/java/fr/eni/springboot/controller/CategoryController.java
+++ src/main/java/fr/eni/springboot/controller/CategoryController.java
@@ -4,11 +4,10 @@ import fr.eni.springboot.bo.Category;
 import fr.eni.springboot.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/admin")
 public class CategoryController {
 
     private final CategoryService categoryService;
@@ -17,12 +16,19 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+
+    @GetMapping("/categoryList")
+    public String displayCategories(Model model) {
+        model.addAttribute("categoryList", categoryService.findAll());
+        return "admin/list-cat";
+    }
+
     @GetMapping("/createCat")
     public String displayCatForm(Model model) {
 
         model.addAttribute("category", new Category());
 
-        return "create-cat";
+        return "admin/create-cat";
     }
 
     @PostMapping("/createCat")
@@ -30,6 +36,12 @@ public class CategoryController {
 
         categoryService.createCategory(category);
 
-        return "redirect:createCat";
+        return "redirect:/admin/createCat";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteCategory(@RequestParam("id") long id) {
+        categoryService.deleteCat(id);
+        return "redirect:/admin/categoryList";
     }
 }
