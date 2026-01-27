4e3d013faf8299807afac527d885f5ea7ec6428b
antonin martel <antonin.mrtl.pr@gmail.com>
2026-01-27 20:13:35 +0100

    fix login

--- src/main/java/fr/eni/springboot/security/SecurityConfiguration.java
+++ src/main/java/fr/eni/springboot/security/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,6 +33,11 @@ public class SecurityConfiguration {
         );
 
         return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     // mise en place de la gestion des droits en fonction des pages affichées
