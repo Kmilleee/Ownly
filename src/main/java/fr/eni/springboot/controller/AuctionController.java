30142a32d556435cfc1e637ff72298e82dbcb9b1
amartel2025 <antonin.mrtl.pr@gmail.com>
2026-02-02 13:41:27 +0100

    fix add photo to auction

--- src/main/java/fr/eni/springboot/controller/AuctionController.java
+++ src/main/java/fr/eni/springboot/controller/AuctionController.java
@@ -3,9 +3,17 @@ package fr.eni.springboot.controller;
 import fr.eni.springboot.service.AuctionService;
 import fr.eni.springboot.service.ItemSoldService;
 import fr.eni.springboot.service.ItemSoldServiceImpl;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class AuctionController {
@@ -22,6 +30,21 @@ public class AuctionController {
     public String displayAuction(Model model){
         model.addAttribute("activePage", "auction");
         return"auction";
+    }
+
+    @GetMapping("/itemsSold-photos/{id}/{imageName}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveImage(@PathVariable("id") String id,
+                                               @PathVariable("imageName") String imageName) {
+
+        Path imagePath = Paths.get("itemsSold-photos", id, imageName);
+        Resource resource = new FileSystemResource(imagePath);
+        if (resource.exists() && resource.isReadable()) {
+            return ResponseEntity.ok().body(resource);
+        } else {
+            System.out.println("Image introuvable ici : " + imagePath.toAbsolutePath());
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/auctionAll")
